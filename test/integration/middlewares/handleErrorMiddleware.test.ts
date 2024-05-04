c62c6ebb042aecb0c7ab32c11f23bcbb6dd43d31
Hitalo Schraiber Loose <hitaloose@gmail.com>
2024-05-03 22:49:15 -0300

    test: handle error middleware

---
+++ test/integration/middlewares/handleErrorMiddleware.test.ts
@@ -0,0 +1,47 @@
+import "express-async-errors";
+
+import express from "express";
+import request from "supertest";
+
+import { handleErrorMiddleware } from "@/middlewares/handleErrorMiddleware";
+import { HttpError } from "@/errors/HttpError";
+
+const app = express();
+app.use(express.json());
+
+app.get("/http_error", () => {
+  throw new HttpError("any-http-error-message", 400);
+});
+
+app.get("/generic_error", () => {
+  throw new Error("any-generic-error-message");
+});
+
+app.get("/unknown_error", () => {
+  throw "unknow-error";
+});
+
+app.use(handleErrorMiddleware);
+
+describe("handleErrorMiddleware", () => {
+  it("should handle with HttpError", async () => {
+    const response = await request(app).get("/http_error");
+
+    expect(response.statusCode).toBe(400);
+    expect(response.body.errorMessage).toBe("any-http-error-message");
+  });
+
+  it("should handle with generic error", async () => {
+    const response = await request(app).get("/generic_error");
+
+    expect(response.statusCode).toBe(500);
+    expect(response.body.errorMessage).toBe("any-generic-error-message");
+  });
+
+  it("should handle with unknown error", async () => {
+    const response = await request(app).get("/unknown_error");
+
+    expect(response.statusCode).toBe(500);
+    expect(response.body.errorMessage).toBe("unknown error");
+  });
+});
