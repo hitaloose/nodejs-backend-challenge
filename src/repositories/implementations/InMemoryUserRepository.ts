e8602b45d4ab6ef7215016b0a854221d8e455fb6
Hitalo Schraiber Loose <hitaloose@gmail.com>
2024-05-05 20:02:35 -0300

    feat: implements in memory user repo

---
+++ src/repositories/implementations/InMemoryUserRepository.ts
@@ -0,0 +1,31 @@
+import { v4 as uuidv4 } from "uuid";
+
+import { User } from "@/models/user";
+import { IUserRepository, Values } from "../interfaces/IUserRepository";
+
+export class InMemoryUserRepository implements IUserRepository {
+  static USERS: User[] = [];
+
+  async insert(values: Values): Promise<User> {
+    const createdUser = {
+      id: uuidv4(),
+      ...values,
+    };
+
+    InMemoryUserRepository.USERS.push(createdUser);
+
+    return createdUser;
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    const user = InMemoryUserRepository.USERS.find(
+      (item) => item.email === email
+    );
+
+    if (!user) {
+      return null;
+    }
+
+    return user;
+  }
+}
